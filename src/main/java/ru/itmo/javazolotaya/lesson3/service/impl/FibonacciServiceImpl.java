e393fbd63c8afc49db163eb278a18e9bb9a9b936
mikhail <korolev.mihail31@rwb.ru>
2025-09-18 09:49:02 +0300

    removed Cacheable + added Map

--- src/main/java/ru/itmo/javazolotaya/lesson3/service/impl/FibonacciServiceImpl.java
+++ src/main/java/ru/itmo/javazolotaya/lesson3/service/impl/FibonacciServiceImpl.java
@@ -1,23 +1,42 @@
 package ru.itmo.javazolotaya.lesson3.service.impl;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import ru.itmo.javazolotaya.lesson3.service.FibonacciService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class FibonacciServiceImpl implements FibonacciService {
 
     private final boolean cacheEnabled;
+    private final Map<Integer, Long> cache = new HashMap<>();
 
     public FibonacciServiceImpl(@Value("${fibonacci.useCache:true}") boolean cacheEnabled) {
         this.cacheEnabled = cacheEnabled;
     }
 
     @Override
-    @Cacheable("fibonacci")
     public long getFibonacci(int n) {
-        System.out.println( "cacheEnabled = " + cacheEnabled);
+        if (!cacheEnabled) {
+            return calculateFibonacci(n);
+        }
+        return getFibonacciCached(n);
+    }
+
+    private long getFibonacciCached(int n) {
+        if (cache.containsKey(n)) {
+            System.out.println("Берём из кеша: F(" + n + ")");
+            return cache.get(n);
+        }
+        long calculated = calculateFibonacci(n);
+        cache.put(n, calculated);
+        return calculated;
+    }
+
+    private long calculateFibonacci(int n) {
+        System.out.println("Вычисляю F(" + n + ") ...");
         if (n < 0) throw new IllegalArgumentException("отрицательное число");
         if (n == 0) return 0;
         if (n == 1) return 1;
